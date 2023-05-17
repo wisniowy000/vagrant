148c2c4e2bb142efaa93f25126cba3bb9731e092
wisniowy000 <42775839+wisniowy000@users.noreply.github.com>
2023-05-17 20:34:40 +0200

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = "2.4.0"
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
     }
   }
 }
 
-provider "local" {
+provider "null" {
   # Configuration options
 }
